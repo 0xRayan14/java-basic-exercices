a814a15160abe14f998b0d39dd299843e6b07dfa
0xRayan14 <rayan.seghrouchni@jobtrek.ch>
2023-10-13 11:55:08 +0200

    finished first CSV exercice

--- src/main/java/ch/jobtrek/Csv.java
+++ src/main/java/ch/jobtrek/Csv.java
@@ -1,10 +1,15 @@
 package ch.jobtrek;
 
+import ch.jobtrek.sbb.Tunnel;
 import ch.jobtrek.sbb.Tunnelable;
 
-import java.io.File;
+import java.io.*;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Csv {
 
@@ -15,20 +20,26 @@ public class Csv {
      * @return A List of objects that implements the Tunnelable interface. You need to create this special class.
      */
     public static List<Tunnelable> importCSVfile(URI filePath) {
-        List<Tunnelable> tunnelableList = new ArrayList<>();
-
-
-        return tunnelableList;
+        try (var lines = Files.lines(Paths.get(filePath))) {
+            return lines.skip(1)
+                    .map(line -> line.split(";"))
+                    .map(tableLines -> new Tunnel(
+                            tableLines[1], Double.parseDouble(tableLines[2]) / 1000, Integer.parseInt(tableLines[3]), tableLines[8]))
+                    .collect(Collectors.toList());
+        } catch (IOException ex) {
+            return List.of();
+        }
     }
 
-    /**
+        /**
      * Sort the list of tunnels provided, and pick de 10 longest
      *
      * @param tunnels A List of tunnels
      * @return A List containing only the 10 longest tunnels
      */
     public static List<Tunnelable> tenLongestTunnels(List<Tunnelable> tunnels) {
-        return List.of(); // Replace with your code here
+
+        return new ArrayList<>(tunnels);
     }
 
     /**
